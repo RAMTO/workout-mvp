89a8ec2283612881098227499afa89138e7b86f2
Martin Dobrev <martindobrev87@gmail.com>
2019-02-06 21:57:23 +0200

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,181 @@
+const gruntTask = require('load-grunt-tasks')
+const sass = require('node-sass')
+const imageminPngquant = require('imagemin-pngquant')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+
+module.exports = (grunt) => {
+  gruntTask(grunt)
+
+  grunt.initConfig({
+
+    browserSync: {
+      bsFiles: {
+        src: [
+          'dist/**/*.css',
+          'dist/**/*.html',
+          'dist/**/*.js'
+        ]
+      },
+      options: {
+        watchTask: true,
+        port: 3333,
+        reloadDebounce: 2000,
+        server: {
+          baseDir: 'dist'
+        }
+      }
+    },
+
+    copy: {
+      jquery: {
+        expand: true,
+        cwd: 'node_modules/jquery/dist/',
+        src: 'jquery.min.js',
+        dest: 'dist/js/'
+      },
+      bootstrap: {
+        expand: true,
+        cwd: 'node_modules/bootstrap/dist/js/',
+        src: 'bootstrap.bundle.min.js',
+        dest: 'dist/js/'
+      },
+      js: {
+        expand: true,
+        cwd: 'src/js/',
+        src: '**/*.js',
+        dest: 'dist/js/'
+      },
+      img: {
+        expand: true,
+        cwd: 'src/img/',
+        src: '**/*.{png,jpg,gif,ico,svg}',
+        dest: 'dist/img/'
+      },
+      css: {
+        expand: true,
+        cwd: 'src/css/',
+        src: '**/*.css',
+        dest: 'dist/css'
+      },
+      fonts: {
+        expand: true,
+        cwd: 'src/fonts/',
+        src: '**/*.{ttf,otf,woff,woff2}',
+        dest: 'dist/fonts/'
+      }
+    },
+
+    nunjucks: {
+      options: {
+        data: grunt.file.readJSON('src/data/data.json'),
+        paths: 'src/templates',
+        configureEnvironment: function (env, nunjucks) {
+          env.addGlobal('todayTimestamp', Date.now());
+        }
+      },
+      render: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/templates/',
+            src: '*.njk',
+            dest: 'dist/',
+            ext: '.html'
+          }
+        ]
+      }
+    },
+
+    sass: {
+      options: {
+        sourceMap: true,
+        implementation: sass,
+        outputStyle: 'expanded'
+      },
+      dist: {
+        files: {
+          'dist/css/style.css': 'src/scss/style.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      dist: {
+        files: {
+          'dist/css/style.css': 'dist/css/style.css'
+        }
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'dist',
+          src: '**/*.html',
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'dist/css',
+          src: ['*.css'],
+          dest: 'dist/css'
+        }]
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        options: {
+          optimizationLevel: 3,
+          use: [imageminPngquant({ quality: 80 }), imageminMozjpeg({ quality: 80 })]
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    watch: {
+      data: {
+        files: 'src/data/data.json',
+        tasks: ['nunjucks']
+      },
+      templates: {
+        files: 'src/**/*.njk',
+        tasks: ['nunjucks']
+      },
+      css: {
+        files: 'src/**/*.scss',
+        tasks: ['sass', 'autoprefixer']
+      },
+      javascript: {
+        files: ['src/**/*.js'],
+        tasks: ['copy:js']
+      },
+      img: {
+        files: 'src/**/*.{png,jpg,gif,ico,svg}',
+        tasks: ['copy:img']
+      }
+    }
+
+  })
+
+  grunt.registerTask('common_prod', ['htmlmin', 'cssmin', 'imagemin'])
+
+  grunt.registerTask('build:prod', ['nunjucks', 'copy', 'sass', 'autoprefixer', 'common_prod'])
+  grunt.registerTask('build:dev', ['nunjucks', 'copy', 'sass', 'autoprefixer'])
+  grunt.registerTask('default', ['browserSync', 'watch'])
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Bootstrap project start
+
+_Bootstrap project start_ is a data driven boilerplate for fast and easy project start.
+
+## Getting Started
+
+### Prerequisites
+
+At least version 8 of Node.js is required for project usage.
+Check your current Node.js version with:
+
+```
+node -v
+```
+
+Use [NVM](https://github.com/creationix/nvm) to upgade Node.js verion if needed.
+
+### Installing
+
+After cloning the project, install dependencies running:
+
+`yarn` or `npm install`
+
+
+To start project:
+
+`yarn start` or  `npm start`
+
+## Step by step guide
+
+The project is data driven, so most of the configuration will be made in _.json_ files
+
+### General setup
+[Nunjucks](https://mozilla.github.io/nunjucks/) is used as a template engine, so the main config file is in _src/data/data.json_. All the meta data is cofigurated from this file.
+
+### SCSS variables
+* Colors
+* Typography
+* Grid
